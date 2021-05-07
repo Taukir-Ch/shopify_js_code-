03e008b22e345147cb706e6e14e939b90c91b9f7
MD Taukir Chowdhury <44375663+Taukir-Ch@users.noreply.github.com>
2021-05-07 17:04:43 +0600

    new update

--- primari-need-code.js
+++ primari-need-code.js
@@ -3,3 +3,20 @@
     $( document ).ready(function() {
         $(".grand_link").parent().parent().parent().addClass('mega-menu').removeClass('submenu');
     });
+
+
+for add to cart click open side bar javascript
+
+
+<script>
+  var myVar;
+  
+  function myFunction() {
+    myVar = setTimeout(alertFunc, 1000);
+  }
+  
+  function alertFunc() {
+  var element = document.getElementById("miniCart");
+  element.classList.add("open");
+}
+  </script>
